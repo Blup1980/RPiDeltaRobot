28ac2ba300c4558f7877b3732a814826e4115bec
Blup1980 <Blup1980@users.noreply.github.com>
2018-04-03 17:01:37 +0200

    modifiex pycnc for the delta robot

--- src/cnc/config.py
+++ src/cnc/config.py
@@ -10,12 +10,6 @@ MIN_VELOCITY_MM_PER_MIN = 1
 # Average velocity for endstop calibration procedure
 CALIBRATION_VELOCITY_MM_PER_MIN = 300
 
-# Stepper motors steps per millimeter for each axis.
-STEPPER_PULSES_PER_MM_X = 100
-STEPPER_PULSES_PER_MM_Y = 100
-STEPPER_PULSES_PER_MM_Z = 400
-STEPPER_PULSES_PER_MM_E = 150
-
 # Invert axises direction, by default(False) high level means increase of
 # position. For inverted(True) axis, high level means decrease of position.
 STEPPER_INVERTED_X = True
@@ -23,57 +17,20 @@ STEPPER_INVERTED_Y = False
 STEPPER_INVERTED_Z = False
 STEPPER_INVERTED_E = True
 
-# Invert zero end stops switches. By default(False) low level on input pin
-# means that axis in zero position. For inverted(True) end stops, high level
-# means zero position.
-ENDSTOP_INVERTED_X = True
-ENDSTOP_INVERTED_Y = True
-ENDSTOP_INVERTED_Z = False  # Auto leveler
-
 # Workplace physical size.
 TABLE_SIZE_X_MM = 200
 TABLE_SIZE_Y_MM = 200
 TABLE_SIZE_Z_MM = 220
 
 # Mixed settings.
-STEPPER_PULSE_LENGTH_US = 2
 STEPPER_MAX_ACCELERATION_MM_PER_S2 = 3000  # for all axis, mm per sec^2
 SPINDLE_MAX_RPM = 10000
-EXTRUDER_MAX_TEMPERATURE = 250
-BED_MAX_TEMPERATURE = 100
-MIN_TEMPERATURE = 40
-EXTRUDER_PID = {"P": 0.059161177519,
-                "I": 0.00206217171374,
-                "D": 0.206217171374}
-BED_PID = {"P": 0.226740848076,
-           "I": 0.00323956215053,
-           "D": 0.323956215053}
 
 # -----------------------------------------------------------------------------
 # Pins configuration.
 
-# Enable pin for all steppers, low level is enabled.
-STEPPERS_ENABLE_PIN = 26
-STEPPER_STEP_PIN_X = 21
-STEPPER_STEP_PIN_Y = 16
-STEPPER_STEP_PIN_Z = 12
-STEPPER_STEP_PIN_E = 8
-
-STEPPER_DIR_PIN_X = 20
-STEPPER_DIR_PIN_Y = 19
-STEPPER_DIR_PIN_Z = 13
-STEPPER_DIR_PIN_E = 7
-
 SPINDLE_PWM_PIN = 4
 FAN_PIN = 27
-EXTRUDER_HEATER_PIN = 18
-BED_HEATER_PIN = 22
-EXTRUDER_TEMPERATURE_SENSOR_CHANNEL = 2
-BED_TEMPERATURE_SENSOR_CHANNEL = 1
-
-ENDSTOP_PIN_X = 23
-ENDSTOP_PIN_Y = 10
-ENDSTOP_PIN_Z = 25
 
 # -----------------------------------------------------------------------------
 #  Behavior config
@@ -90,5 +47,3 @@ INSTANT_RUN = False
 # velocity.
 AUTO_VELOCITY_ADJUSTMENT = True
 
-# Automatically turn on fan when extruder is heating, boolean value.
-AUTO_FAN_ON = True
--- src/cnc/gmachine.py
+++ src/cnc/gmachine.py
@@ -1,10 +1,8 @@
 from __future__ import division
 
 import cnc.logging_config as logging_config
-from cnc import hal
 from cnc.pulses import *
 from cnc.coordinates import *
-from cnc.heater import *
 from cnc.enums import *
 from cnc.watchdog import *
 
@@ -19,7 +17,6 @@ class GMachine(object):
     """ Main object which control and keep state of whole machine: steppers,
         spindle, extruder etc
     """
-    AUTO_FAN_ON = AUTO_FAN_ON
 
     def __init__(self):
         """ Initialization.
@@ -30,10 +27,7 @@ class GMachine(object):
         self._spindle_rpm = 0
         self._local = None
         self._convertCoordinates = 0
-        self._absoluteCoordinates = 0
         self._plane = None
-        self._fan_state = False
-        self._heaters = dict()
         self.reset()
         hal.init()
         self.watchdog = HardwareWatchdog()
@@ -42,9 +36,6 @@ class GMachine(object):
         """ Free all resources.
         """
         self._spindle(0)
-        for h in self._heaters:
-            self._heaters[h].stop()
-        self._fan(False)
         hal.deinit()
 
     def reset(self):
@@ -57,51 +48,12 @@ class GMachine(object):
         self._spindle_rpm = 1000
         self._local = Coordinates(0.0, 0.0, 0.0, 0.0)
         self._convertCoordinates = 1.0
-        self._absoluteCoordinates = True
         self._plane = PLANE_XY
 
     # noinspection PyMethodMayBeStatic
     def _spindle(self, spindle_speed):
         hal.join()
         hal.spindle_control(100.0 * spindle_speed / SPINDLE_MAX_RPM)
-
-    def _fan(self, state):
-        hal.fan_control(state)
-        self._fan_state = state
-
-    def _heat(self, heater, temperature, wait):
-        # check if sensor is ok
-        if heater == HEATER_EXTRUDER:
-            measure = hal.get_extruder_temperature
-            control = hal.extruder_heater_control
-            coefficients = EXTRUDER_PID
-        elif heater == HEATER_BED:
-            measure = hal.get_bed_temperature
-            control = hal.bed_heater_control
-            coefficients = BED_PID
-        else:
-            raise GMachineException("unknown heater")
-        try:
-            measure()
-        except (IOError, OSError):
-            raise GMachineException("can not measure temperature")
-        if heater in self._heaters:
-            self._heaters[heater].stop()
-            del self._heaters[heater]
-        if temperature != 0:
-            if heater == HEATER_EXTRUDER and self.AUTO_FAN_ON:
-                self._fan(True)
-            self._heaters[heater] = Heater(temperature, coefficients, measure,
-                                           control)
-            if wait:
-                self._heaters[heater].wait()
-
-    def __check_delta(self, delta):
-        pos = self._position + delta
-        if not pos.is_in_aabb(Coordinates(0.0, 0.0, 0.0, 0.0),
-                              Coordinates(TABLE_SIZE_X_MM, TABLE_SIZE_Y_MM,
-                                          TABLE_SIZE_Z_MM, 0)):
-            raise GMachineException("out of effective area")
 
     # noinspection PyMethodMayBeStatic
     def __check_velocity(self, max_velocity):
@@ -111,21 +63,21 @@ class GMachine(object):
                 or max_velocity.e > MAX_VELOCITY_MM_PER_MIN_E:
             raise GMachineException("out of maximum speed")
 
-    def _move_linear(self, delta, velocity):
-        delta = delta.round(1.0 / STEPPER_PULSES_PER_MM_X,
-                            1.0 / STEPPER_PULSES_PER_MM_Y,
-                            1.0 / STEPPER_PULSES_PER_MM_Z,
-                            1.0 / STEPPER_PULSES_PER_MM_E)
+    def _move_linear(self, new_pos, velocity):
+        delta = new_pos - self._position
         if delta.is_zero():
             return
-        self.__check_delta(delta)
+        if not new_pos.is_in_aabb(Coordinates(0.0, 0.0, 0.0, 0.0),
+                                  Coordinates(TABLE_SIZE_X_MM, TABLE_SIZE_Y_MM,
+                                              TABLE_SIZE_Z_MM, 0)):
+            raise GMachineException("out of effective area")
 
-        logging.info("Moving linearly {}".format(delta))
-        gen = PulseGeneratorLinear(delta, velocity)
+        logging.info("Moving linearly to{}".format(new_pos))
+        gen = PulseGeneratorLinear(delta, new_pos, velocity)
         self.__check_velocity(gen.max_velocity())
         hal.move(gen)
         # save position
-        self._position = self._position + delta
+        self._position = new_pos
 
     @staticmethod
     def __quarter(pa, pb):
@@ -185,65 +137,6 @@ class GMachine(object):
             pq = q
         return ea, eb
 
-    def _move_circular(self, delta, radius, velocity, direction):
-        delta = delta.round(1.0 / STEPPER_PULSES_PER_MM_X,
-                            1.0 / STEPPER_PULSES_PER_MM_Y,
-                            1.0 / STEPPER_PULSES_PER_MM_Z,
-                            1.0 / STEPPER_PULSES_PER_MM_E)
-        radius = radius.round(1.0 / STEPPER_PULSES_PER_MM_X,
-                              1.0 / STEPPER_PULSES_PER_MM_Y,
-                              1.0 / STEPPER_PULSES_PER_MM_Z,
-                              1.0 / STEPPER_PULSES_PER_MM_E)
-        self.__check_delta(delta)
-        # get delta vector and put it on circle
-        circle_end = Coordinates(0, 0, 0, 0)
-        if self._plane == PLANE_XY:
-            circle_end.x, circle_end.y = \
-                self.__adjust_circle(delta.x, delta.y, radius.x, radius.y,
-                                     direction, self._position.x,
-                                     self._position.y, TABLE_SIZE_X_MM,
-                                     TABLE_SIZE_Y_MM)
-            circle_end.z = delta.z
-        elif self._plane == PLANE_YZ:
-            circle_end.y, circle_end.z = \
-                self.__adjust_circle(delta.y, delta.z, radius.y, radius.z,
-                                     direction, self._position.y,
-                                     self._position.z, TABLE_SIZE_Y_MM,
-                                     TABLE_SIZE_Z_MM)
-            circle_end.x = delta.x
-        elif self._plane == PLANE_ZX:
-            circle_end.z, circle_end.x = \
-                self.__adjust_circle(delta.z, delta.x, radius.z, radius.x,
-                                     direction, self._position.z,
-                                     self._position.x, TABLE_SIZE_Z_MM,
-                                     TABLE_SIZE_X_MM)
-            circle_end.y = delta.y
-        circle_end.e = delta.e
-        circle_end = circle_end.round(1.0 / STEPPER_PULSES_PER_MM_X,
-                                      1.0 / STEPPER_PULSES_PER_MM_Y,
-                                      1.0 / STEPPER_PULSES_PER_MM_Z,
-                                      1.0 / STEPPER_PULSES_PER_MM_E)
-        logging.info("Moving circularly {} {} {} with radius {}"
-                     " and velocity {}".format(self._plane, circle_end,
-                                               direction, radius, velocity))
-        gen = PulseGeneratorCircular(circle_end, radius, self._plane,
-                                     direction, velocity)
-        self.__check_velocity(gen.max_velocity())
-        # if finish coords is not on circle, move some distance linearly
-        linear_delta = delta - circle_end
-        linear_gen = None
-        if not linear_delta.is_zero():
-            logging.info("Moving additionally {} to finish circle command".
-                         format(linear_delta))
-            linear_gen = PulseGeneratorLinear(linear_delta, velocity)
-            self.__check_velocity(linear_gen.max_velocity())
-        # do movements
-        hal.move(gen)
-        if linear_gen is not None:
-            hal.move(linear_gen)
-        # save position
-        self._position = self._position + circle_end + linear_delta
-
     def safe_zero(self, x=True, y=True, z=True):
         """ Move head to zero position safely.
         :param x: boolean, move X axis to zero
@@ -251,18 +144,29 @@ class GMachine(object):
         :param z: boolean, move Z axis to zero
         """
         if x and not y:
-            self._move_linear(Coordinates(-self._position.x, 0, 0, 0),
+            self._move_linear(Coordinates(0,
+                                          self._position.y,
+                                          self._position.z,
+                                          self._position.e),
                               MAX_VELOCITY_MM_PER_MIN_X)
         elif y and not x:
-            self._move_linear(Coordinates(0, -self._position.y, 0, 0),
+            self._move_linear(Coordinates(self._position.x,
+                                          0,
+                                          self._position.z,
+                                          self._position.e),
                               MAX_VELOCITY_MM_PER_MIN_X)
         elif x and y:
-            d = Coordinates(-self._position.x, -self._position.y, 0, 0)
-            self._move_linear(d, min(MAX_VELOCITY_MM_PER_MIN_X,
-                                     MAX_VELOCITY_MM_PER_MIN_Y))
+            self._move_linear(Coordinates(0,
+                                          0,
+                                          0,
+                                          self._position.e),
+                              min(MAX_VELOCITY_MM_PER_MIN_X,
+                                  MAX_VELOCITY_MM_PER_MIN_Y))
         if z:
-            d = Coordinates(0, 0, -self._position.z, 0)
-            self._move_linear(d, MAX_VELOCITY_MM_PER_MIN_Z)
+            self._move_linear(Coordinates(self._position.x,
+                                          self._position.y,
+                                          0, self._position.e),
+                              MAX_VELOCITY_MM_PER_MIN_Z)
 
     def position(self):
         """ Return current machine position (after the latest command)
@@ -281,29 +185,6 @@ class GMachine(object):
         """
         return self._plane
 
-    def fan_state(self):
-        """ Check if fan is on.
-            :return True if fan is on, False otherwise.
-        """
-        return self._fan_state
-
-    def __get_target_temperature(self, heater):
-        if heater not in self._heaters:
-            return 0
-        return self._heaters[heater].target_temperature()
-
-    def extruder_target_temperature(self):
-        """ Return desired extruder temperature.
-            :return Temperature in Celsius, 0 if disabled.
-        """
-        return self.__get_target_temperature(HEATER_EXTRUDER)
-
-    def bed_target_temperature(self):
-        """ Return desired bed temperature.
-            :return Temperature in Celsius, 0 if disabled.
-        """
-        return self.__get_target_temperature(HEATER_BED)
-
     def do_command(self, gcode):
         """ Perform action.
         :param gcode: GCode object which represent one gcode line
@@ -318,18 +199,13 @@ class GMachine(object):
         if c is None and gcode.has_coordinates():
             c = 'G1'
         # read parameters
-        if self._absoluteCoordinates:
-            coord = gcode.coordinates(self._position - self._local,
-                                      self._convertCoordinates)
-            coord = coord + self._local
-            delta = coord - self._position
-        else:
-            delta = gcode.coordinates(Coordinates(0.0, 0.0, 0.0, 0.0),
-                                      self._convertCoordinates)
-            # coord = self._position + delta
+
+        coord = gcode.coordinates(self._position - self._local,
+                                  self._convertCoordinates)
+        coord = coord + self._local
+        delta = coord - self._position
+
         velocity = gcode.get('F', self._velocity)
-        radius = gcode.radius(Coordinates(0.0, 0.0, 0.0, 0.0),
-                              self._convertCoordinates)
         # check parameters
         if velocity < MIN_VELOCITY_MM_PER_MIN:
             raise GMachineException("feed speed too low")
@@ -339,9 +215,9 @@ class GMachine(object):
                      MAX_VELOCITY_MM_PER_MIN_Y,
                      MAX_VELOCITY_MM_PER_MIN_Z,
                      MAX_VELOCITY_MM_PER_MIN_E)
-            l = delta.length()
-            if l > 0:
-                proportion = abs(delta) / l
+            length = delta.length()
+            if length > 0:
+                proportion = abs(delta) / length
                 if proportion.x > 0:
                     v = int(MAX_VELOCITY_MM_PER_MIN_X / proportion.x)
                     if v < vl:
@@ -358,13 +234,9 @@ class GMachine(object):
                     v = int(MAX_VELOCITY_MM_PER_MIN_E / proportion.e)
                     if v < vl:
                         vl = v
-            self._move_linear(delta, vl)
+            self._move_linear(coord, vl)
         elif c == 'G1':  # linear interpolation
-            self._move_linear(delta, velocity)
-        elif c == 'G2':  # circular interpolation, clockwise
-            self._move_circular(delta, radius, velocity, CW)
-        elif c == 'G3':  # circular interpolation, counterclockwise
-            self._move_circular(delta, radius, velocity, CCW)
+            self._move_linear(coord, velocity)
         elif c == 'G4':  # delay in s
             if not gcode.has('P'):
                 raise GMachineException("P is not specified")
@@ -394,9 +266,9 @@ class GMachine(object):
         elif c == 'G53':  # switch to machine coords
             self._local = Coordinates(0.0, 0.0, 0.0, 0.0)
         elif c == 'G90':  # switch to absolute coords
-            self._absoluteCoordinates = True
+            pass
         elif c == 'G91':  # switch to relative coords
-            self._absoluteCoordinates = False
+            raise GMachineException("Not supported")
         elif c == 'G92':  # switch to local coords
             if gcode.has_coordinates():
                 self._local = self._position - gcode.coordinates(
@@ -419,42 +291,6 @@ class GMachine(object):
             self.reset()
         elif c == 'M84':  # disable motors
             hal.disable_steppers()
-        # extruder and bed heaters control
-        elif c == 'M104' or c == 'M109' or c == 'M140' or c == 'M190':
-            if c == 'M104' or c == 'M109':
-                heater = HEATER_EXTRUDER
-            elif c == 'M140' or c == 'M190':
-                heater = HEATER_BED
-            else:
-                raise Exception("Unexpected heater command")
-            wait = c == 'M109' or c == 'M190'
-            if not gcode.has("S"):
-                raise GMachineException("temperature is not specified")
-            t = gcode.get('S', 0)
-            if ((heater == HEATER_EXTRUDER and t > EXTRUDER_MAX_TEMPERATURE) or
-                    (heater == HEATER_BED and t > BED_MAX_TEMPERATURE) or
-                    t < MIN_TEMPERATURE) and t != 0:
-                raise GMachineException("bad temperature")
-            self._heat(heater, t, wait)
-        elif c == 'M105':  # get temperature
-            try:
-                et = hal.get_extruder_temperature()
-            except (IOError, OSError):
-                et = None
-            try:
-                bt = hal.get_bed_temperature()
-            except (IOError, OSError):
-                bt = None
-            if et is None and bt is None:
-                raise GMachineException("can not measure temperature")
-            answer = "E:{} B:{}".format(et, bt)
-        elif c == 'M106':  # fan control
-            if gcode.get('S', 1) != 0:
-                self._fan(True)
-            else:
-                self._fan(False)
-        elif c == 'M107':  # turn off fan
-            self._fan(False)
         elif c == 'M111':  # enable debug
             logging_config.debug_enable()
         elif c == 'M114':  # get current position
@@ -463,13 +299,6 @@ class GMachine(object):
             answer = "X:{} Y:{} Z:{} E:{}".format(p.x, p.y, p.z, p.e)
         elif c is None:  # command not specified(ie just F was passed)
             pass
-        # commands below are added just for compatibility
-        elif c == 'M82':  # absolute mode for extruder
-            if not self._absoluteCoordinates:
-                raise GMachineException("Not supported, use G90/G91")
-        elif c == 'M83':  # relative mode for extruder
-            if self._absoluteCoordinates:
-                raise GMachineException("Not supported, use G90/G91")
         else:
             raise GMachineException("unknown command")
         # save parameters on success
